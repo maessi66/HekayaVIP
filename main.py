6394e0da751af27d9032ff8039954620c03b912d
maessi66 <43505049+maessi66@users.noreply.github.com>
2025-11-19 00:59:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
+# main.py
 import webbrowser
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 
-# Load KV file
+# Load KV layout
 Builder.load_file("app.kv")
 
 class MyScreenManager(ScreenManager):
     pass
 
 class MainApp(App):
-
     def build(self):
         return MyScreenManager()
 
     def open_telegram(self):
+        # افتح رابط التليجرام المرسل منك
         webbrowser.open("https://t.me/maessi2012")
 
     def logout(self):
-        self.root.current = "login"
-
+        # اعد الى شاشة الدخول
+        # بعض الشاشات يستخدمون self.root.manager او self.root ... نستخدم root الحالية
+        try:
+            self.root.current = "login"
+        except Exception:
+            # بديل عام
+            for s in self.root.screens:
+                if s.name == "login":
+                    self.root.current = "login"
+                    break
 
 if __name__ == "__main__":
     MainApp().run()
